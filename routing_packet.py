a4ae8f41adcd920d14f3aed41b3a94c5dac9e1ed
wjq2019ustc <3194839112@qq.com>
2023-04-13 15:55:07 +0800

    test

---
+++ routing_packet.py
@@ -0,0 +1,13 @@
+from qns.entity.node.app import Application
+from qns.network.requests import Request
+
+
+class SendroutingApp(Application):
+    def __init__(self, request_list: list[Request] = []):
+        super().__init__()
+
+
+
+class RecvroutingApp(Application):
+    def __init__(self, request_list: list[Request] = []):
+        super().__init__()
